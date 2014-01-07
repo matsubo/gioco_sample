d39ed21edc26a65beb770fa63082a4fd33815175
Yuki Matsukura <matsubokkuri@gmail.com>
2014-01-07 19:18:30 +0900

    Until generated gioco setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A::Application.config.secret_key_base = '76782c28dcbfae9c19a3431ee0aa8402e702a44dcf4ac62d77955e75f2f6e8aeaf7537062b3c261e7cf5d907035dc327a4a47991e81f129fd6415416c3a029d5'
